a13cbf0a51ac07f1bdc4d2a1f5d8ff1f38139329
Martin Roed <msroed@gmail.com>
2023-03-08 10:12:56 +0100

    feat: browser is king

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -2,28 +2,29 @@ import MonacoEditor, { loader, OnChange, OnMount } from "@monaco-editor/react";
 import * as monacoEditor from "monaco-editor";
 import { useRef } from "react";
 
+// Configure Monaco editor
 loader.config({ monaco: monacoEditor });
-
-monacoEditor.languages.register({
-	id: "python",
-	extensions: [".py"],
-});
+monacoEditor.languages.register({ id: "python", extensions: [".py"] });
 
 interface Props {
 	onChange?: (code: string | undefined) => void | Promise<void>;
 }
 
+// Default code to show in the editor
 const DEFAULT_CODE = `import databutton as db
 import streamlit as st
 import pandas as pd`;
 
 export const Editor = ({ onChange }: Props) => {
+	// Use a ref to keep track of the editor instance
 	const ref = useRef<monacoEditor.editor.IStandaloneCodeEditor>();
 
+	// Called when the editor is mounted
 	const handleDidMount: OnMount = (editor) => {
 		ref.current = editor;
 	};
 
+	// Called when the code changes
 	const handleChange: OnChange = (value, event) => {
 		onChange?.(value);
 	};
